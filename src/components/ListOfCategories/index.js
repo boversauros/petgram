66aeb328dea0c965182487bddce7e72820bf8e1a
Oriol <bover73@gmail.com>
2019-08-13 19:51:43 +0200

    fetch category data

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
+import { categories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
   return (
     <List>
       {
-        [1, 2, 3, 4, 5, 6].map(category => <Item key={category} ><Category /></Item>)
+        categories.map(category => <Item key={category.id} ><Category {...category} /></Item>)
       }
     </List>
   )
