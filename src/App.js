45ee100e68eb2a79d749c4b027e965ae1ed87bbb
Oriol <bover73@gmail.com>
2019-08-14 18:51:20 +0200

    create styles folder and animations

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
-import { GlobalStyle } from './GlobalStyles'
+import { Logo } from './components/Logo'
+import { GlobalStyle } from './styles/GlobalStyles'
 
 export const App = () => (
   <div>
     <GlobalStyle />
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
   </div>
