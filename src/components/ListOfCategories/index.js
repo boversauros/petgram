18a7b490d19e6c27ca75cdba4a8c6582323d5efc
Oriol <bover73@gmail.com>
2019-08-15 13:00:43 +0200

    add hooks and axios

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Category } from '../Category'
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    axios.get('https://petgram-api-boversauros.now.sh/categories')
+      .then(({ data }) => setCategories(data))
+  }, [])
+
   return (
     <List>
       {
