b84b67d7b8a998dd171c12afcb344c2715b280b4
Oriol <bover73@gmail.com>
2019-08-27 20:26:34 +0200

    add context hook

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,48 +1,49 @@
-import React, { Fragment } from 'react'
-import Context from '../Context'
+import React, { Fragment, useContext } from 'react'
+import { Context } from '../Context'
 import { UserForm } from '../components/UserForm'
 import { RegisterMutation } from '../container/RegisterMutation'
 import { LoginMutation } from '../container/LoginMutation'
 
-export const NotRegisteredUser = () => (
-  <Context.Consumer>
-    {
-      ({ activateAuth }) => {
-        return (
-          <Fragment>
-            <RegisterMutation>
-              {
-                (register, { data, loading, error }) => {
-                  const onSubmit = ({ email, password }) => {
-                    const input = { email, password }
-                    const variables = { input }
-                    register({ variables }).then(activateAuth)
-                  }
+export const NotRegisteredUser = () => {
+  const { activateAuth } = useContext(Context)
+  return (
+    <Fragment>
+      <RegisterMutation>
+        {
+          (register, { data, loading, error }) => {
+            const onSubmit = ({ email, password }) => {
+              const input = { email, password }
+              const variables = { input }
+              register({ variables }).then(({ data }) => {
+                const { signup } = data
+                activateAuth(signup)
+              })
+            }
 
-                  const errorMsg = error && `Hay algun problema con el registro`
+            const errorMsg = error && `Hay algun problema con el registro`
 
-                  return <UserForm disable={loading} error={errorMsg} onSubmit={onSubmit} title='Registrarse' />
-                }
-              }
-            </RegisterMutation>
-            <LoginMutation>
-              {
-                (login, { data, loading, error }) => {
-                  const onSubmit = ({ email, password }) => {
-                    const input = { email, password }
-                    const variables = { input }
-                    login({ variables }).then(activateAuth)
-                  }
+            return <UserForm disable={loading} error={errorMsg} onSubmit={onSubmit} title='Registrarse' />
+          }
+        }
+      </RegisterMutation>
+      <LoginMutation>
+        {
+          (login, { data, loading, error }) => {
+            const onSubmit = ({ email, password }) => {
+              const input = { email, password }
+              const variables = { input }
+              login({ variables }).then(({ data }) => {
+                const { login } = data
+                activateAuth(login)
+              })
+            }
 
-                  const errorMsg = error && `Credenciales erroneas`
+            const errorMsg = error && `Credenciales erroneas`
 
-                  return <UserForm disable={loading} error={errorMsg} onSubmit={onSubmit} title='Iniciar sesión' />
-                }
-              }
-            </LoginMutation>
-          </Fragment>
-        )
-      }
-    }
-  </Context.Consumer>
-)
+            return <UserForm disable={loading} error={errorMsg} onSubmit={onSubmit} title='Iniciar sesión' />
+          }
+        }
+      </LoginMutation>
+    </Fragment>
+  )
+}
