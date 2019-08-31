323c6768534dd057bd5fceef109aeb78f9d6e9b2
Oriol <bover73@gmail.com>
2019-08-31 12:23:50 +0200

    refactor mutations hooks

--- src/container/LoginMutation.js
+++ src/container/LoginMutation.js
@@ -1,14 +1,29 @@
-import React from 'react'
-import { Mutation } from 'react-apollo'
+import React, { useContext } from 'react'
+import { Context } from '../Context'
+import { UserForm } from '../components/UserForm'
 import { gql } from 'apollo-boost'
+import { useMutation } from '@apollo/react-hooks'
 
 const LOGIN = gql`
 mutation login($input: UserCredentials!) {
     login (input: $input)
 }
 `
-export const LoginMutation = ({ children }) => {
-  return <Mutation mutation={LOGIN} >
-    {children}
-  </Mutation>
+
+export const Login = () => {
+  const [login, { loading, error }] = useMutation(LOGIN)
+  const { activateAuth } = useContext(Context)
+
+  const onSubmit = ({ email, password }) => {
+    const input = { email, password }
+    const variables = { input }
+    login({ variables }).then(({ data }) => {
+      const { login } = data
+      activateAuth(login)
+    })
+  }
+
+  const errorMsg = error && `Credenciales erroneas`
+
+  return <UserForm disable={loading} error={errorMsg} onSubmit={onSubmit} title='Iniciar sesión' />
 }
--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,49 +1,12 @@
-import React, { Fragment, useContext } from 'react'
-import { Context } from '../Context'
-import { UserForm } from '../components/UserForm'
-import { RegisterMutation } from '../container/RegisterMutation'
-import { LoginMutation } from '../container/LoginMutation'
+import React, { Fragment } from 'react'
+import { Register } from '../container/RegisterMutation'
+import { Login } from '../container/LoginMutation'
 
 export const NotRegisteredUser = () => {
-  const { activateAuth } = useContext(Context)
   return (
     <Fragment>
-      <RegisterMutation>
-        {
-          (register, { data, loading, error }) => {
-            const onSubmit = ({ email, password }) => {
-              const input = { email, password }
-              const variables = { input }
-              register({ variables }).then(({ data }) => {
-                const { signup } = data
-                activateAuth(signup)
-              })
-            }
-
-            const errorMsg = error && `Hay algun problema con el registro`
-
-            return <UserForm disable={loading} error={errorMsg} onSubmit={onSubmit} title='Registrarse' />
-          }
-        }
-      </RegisterMutation>
-      <LoginMutation>
-        {
-          (login, { data, loading, error }) => {
-            const onSubmit = ({ email, password }) => {
-              const input = { email, password }
-              const variables = { input }
-              login({ variables }).then(({ data }) => {
-                const { login } = data
-                activateAuth(login)
-              })
-            }
-
-            const errorMsg = error && `Credenciales erroneas`
-
-            return <UserForm disable={loading} error={errorMsg} onSubmit={onSubmit} title='Iniciar sesión' />
-          }
-        }
-      </LoginMutation>
+      <Register />
+      <Login />
     </Fragment>
   )
 }
